d6cb8d8575ea3e7f72bf8b909034e403c4836ae8
Robin Rojas <robinrojasramirez12@gmail.com>
2025-03-15 18:47:24 -0500

    feat: update main.ts

--- tareas-backend/src/main.ts
+++ tareas-backend/src/main.ts
@@ -6,7 +6,7 @@ async function bootstrap() {
 
   // Configuración de CORS (antes de iniciar el servidor)
   app.enableCors({
-    origin: 'http://localhost:4200', // Permitir peticiones desde Angular
+    origin: ['http://localhost:4200', 'https://prueba-tareas-vdw4.vercel.app'], // Agrega el dominio de producción
     methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
     allowedHeaders: 'Content-Type, Authorization',
     credentials: true, // Si usas cookies o auth con credenciales
